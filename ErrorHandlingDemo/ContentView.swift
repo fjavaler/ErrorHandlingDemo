60fd01a5a8082c6c01394046a370e8eb8709b2f5
Frederick Javalera <frederickjavalera@gmail.com>
2021-10-27 21:19:14 -0600

    Initial Commit

---
+++ ErrorHandlingDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ErrorHandlingDemo
+//
+//  Created by Frederick Javalera on 10/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ErrorHandlingDemo/ErrorHandlingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHandlingDemoApp.swift
+//  ErrorHandlingDemo
+//
+//  Created by Frederick Javalera on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHandlingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
